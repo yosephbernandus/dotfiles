bf68753ee6910191481c1fc4d9651abeef5cd614
yosephbernandus <yosephbernandus@gmail.com>
2025-01-09 23:59:48 +0700

    avante config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -225,7 +225,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+--    See `:help lazy.nvtim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -1448,31 +1448,182 @@ require('lazy').setup({
   --
   -- vim.keymap.set('n', '<A-z>', '<Cmd>BufferScrollLeft<CR>', { desc = 'Scroll left' }),
   -- vim.keymap.set('n', '<A-m>', '<Cmd>BufferScrollRight<CR>', { desc = 'Scroll rigth' }),
-  --
+
+  -- copilot lua vim
+
+  -- copilot
+  {
+    'zbirenbaum/copilot.lua',
+    lazy = false,
+    event = 'InsertEnter',
+    cmd = 'Copilot',
+    config = function()
+      require('copilot').setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = true, -- Enable auto-suggestions
+        },
+        panel = { enabled = true }, -- Enable the Copilot panel
+      }
+    end,
+  },
+  -- end copilot vim
+
+  -- avante AI
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    lazy = false,
+    version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
+    opts = {
+      -- add any opts here
+    },
+    dependencies = {
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+    config = function()
+      require('avante').setup {
+        ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+        provider = 'copilot', -- Recommend using Claude
+        auto_suggestions_provider = 'copilot', -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+        claude = {
+          model = 'claude-3-5-sonnet',
+        },
+        mappings = {
+          --- @class AvanteConflictMappings
+          diff = {
+            ours = 'co',
+            theirs = 'ct',
+            all_theirs = 'ca',
+            both = 'cb',
+            cursor = 'cc',
+            next = ']x',
+            prev = '[x',
+          },
+          suggestion = {
+            accept = '<M-l>',
+            next = '<M-]>',
+            prev = '<M-[>',
+            dismiss = '<C-]>',
+          },
+          jump = {
+            next = ']]',
+            prev = '[[',
+          },
+          submit = {
+            normal = '<CR>',
+            insert = '<C-s>',
+          },
+          sidebar = {
+            apply_all = 'A',
+            apply_cursor = 'a',
+            switch_windows = '<Tab>',
+            reverse_switch_windows = '<S-Tab>',
+          },
+        },
+        hints = { enabled = true },
+        windows = {
+          ---@type "right" | "left" | "top" | "bottom"
+          position = 'right', -- the position of the sidebar
+          wrap = true, -- similar to vim.o.wrap
+          width = 30, -- default % based on available width
+          sidebar_header = {
+            enabled = true, -- true, false to enable/disable the header
+            align = 'center', -- left, center, right for title
+            rounded = true,
+          },
+          input = {
+            prefix = '> ',
+            height = 8, -- Height of the input window in vertical layout
+          },
+          edit = {
+            border = 'rounded',
+            start_insert = true, -- Start insert mode when opening the edit window
+          },
+          ask = {
+            floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+            start_insert = true, -- Start insert mode when opening the ask window
+            border = 'rounded',
+            ---@type "ours" | "theirs"
+            focus_on_apply = 'ours', -- which diff to focus after applying
+          },
+        },
+        highlights = {
+          ---@type AvanteConflictHighlights
+          diff = {
+            current = 'DiffText',
+            incoming = 'DiffAdd',
+          },
+        },
+        --- @class AvanteConflictUserConfig
+        diff = {
+          autojump = true,
+          ---@type string | fun(): any
+          list_opener = 'copen',
+          --- Override the 'timeoutlen' setting while hovering over a diff (see :help timeoutlen).
+          --- Helps to avoid entering operator-pending mode with diff mappings starting with `c`.
+          --- Disable by setting to -1.
+          override_timeoutlen = 500,
+        },
+      }
+    end,
+  },
+  -- end avante AI
 
   -- Codeium AI Completion
   -- running :Codeium Auth for login the token
-  {
-    'Exafunction/codeium.vim',
-    config = function()
-      -- Change '<C-g>' here to any keycode you like.
-      vim.keymap.set('i', '<C-g>', function()
-        return vim.fn['codeium#Accept']()
-      end, { expr = true, silent = true })
-      vim.keymap.set('i', '<C-;>', function()
-        return vim.fn['codeium#CycleCompletions'](1)
-      end, { expr = true, silent = true })
-      vim.keymap.set('i', '<C-,>', function()
-        return vim.fn['codeium#CycleCompletions'](-1)
-      end, { expr = true, silent = true })
-      vim.keymap.set('i', '<C-x>', function()
-        return vim.fn['codeium#Clear']()
-      end, { expr = true, silent = true })
-      vim.keymap.set('i', '<A-ch>', function()
-        return vim.fn['codeium#Chat']()
-      end, { expr = true, silent = true, desc = 'Chat with AI (Codeium)' })
-    end,
-  },
+  -- {
+  --   'Exafunction/codeium.vim',
+  --   config = function()
+  --     -- Change '<C-g>' here to any keycode you like.
+  --     vim.keymap.set('i', '<C-g>', function()
+  --       return vim.fn['codeium#Accept']()
+  --     end, { expr = true, silent = true })
+  --     vim.keymap.set('i', '<C-;>', function()
+  --       return vim.fn['codeium#CycleCompletions'](1)
+  --     end, { expr = true, silent = true })
+  --     vim.keymap.set('i', '<C-,>', function()
+  --       return vim.fn['codeium#CycleCompletions'](-1)
+  --     end, { expr = true, silent = true })
+  --     vim.keymap.set('i', '<C-x>', function()
+  --       return vim.fn['codeium#Clear']()
+  --     end, { expr = true, silent = true })
+  --     vim.keymap.set('i', '<A-ch>', function()
+  --       return vim.fn['codeium#Chat']()
+  --     end, { expr = true, silent = true, desc = 'Chat with AI (Codeium)' })
+  --   end,
+  -- },
   -- End Codeium AI Completion
 
   -- -- tab bar
